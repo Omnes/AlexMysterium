b05b0adff9b32a9eabbfbb8cd1c96a1783fd7bfe
Sean <conk3r_xd@hotmail.com>
2013-08-14 16:25:44 +0200

    yesssa

--- Assets/Scripts/CameraSmoothFollowScript.cs
+++ Assets/Scripts/CameraSmoothFollowScript.cs
@@ -6,20 +6,19 @@ public class CameraSmoothFollowScript : MonoBehaviour {
 	//Should be prefab and should initaiate new player each scene ***************************
 	public GameObject Player;
 	public float CameraHeightZ;
+	public float CameraHeightY;
 	private Vector3 playerMovement;
 	private Vector3 ContactDir;
 	private bool isColliding = false;
-	private Vector3 ScreenMiddle;
-	private Vector3 ScreenMiddleToPlayer;
+	private Vector3 cameraCenter;
+	private Vector3 cameraCenterToPlayer;
 	
 	
 	// Use this for initialization
 	void Start () {
-	
-		playerMovement = new Vector3(Player.transform.position.x, 0.0f, CameraHeightZ);
+		//startposition
+		playerMovement = new Vector3(Player.transform.position.x, CameraHeightY, CameraHeightZ);
 		transform.position = playerMovement;
-
-		
 	}
 	
 	// Update is called once per frame
@@ -31,48 +30,32 @@ public class CameraSmoothFollowScript : MonoBehaviour {
 	
 	void OnCollisionEnter(Collision other) {
 		
-<<<<<<< HEAD
-		//ContactDir = transform.position - other.transform.position;
-=======
+		//collision vector.
 		ContactDir = other.transform.position - transform.position;
->>>>>>> 06be7c0fc38f9a43ff41171c58ba1627c6cea782
-		
-		//ContactDir = other.contacts[0].normal;
-		
-		//Debug.Log("Collide");
-		
+		//is the camera colliding ?
 		isColliding = true;
-		
-		
-	}
-	
-	void OnCollisionStay(){
-		
 		
 	}
 	
 	void SmoothFollow(){
 		
+		//follow player
 		if(isColliding == false){
-			playerMovement = new Vector3(Player.transform.position.x, 0.0f, CameraHeightZ);
+			playerMovement = new Vector3(Player.transform.position.x, CameraHeightY, CameraHeightZ);
 			transform.position = playerMovement;
 		}
 		
-		ScreenMiddle = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0);
+		//Center of camera
+		cameraCenter = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0);
 		
-<<<<<<< HEAD
-		Debug.Log(ScreenMiddleToPlayer);
-=======
-		ScreenMiddleToPlayer = ScreenMiddle - Player.transform.position;
->>>>>>> 06be7c0fc38f9a43ff41171c58ba1627c6cea782
-		
-		Debug.Log("ScreenMiddleToPlayer: "+ScreenMiddleToPlayer);
-		Debug.Log("ContactDir: "+ContactDir);
+		//vector between center and player
+		cameraCenterToPlayer = cameraCenter - Player.transform.position;
 	
-		if(ScreenMiddleToPlayer.x > 0 && ContactDir.x > 0){
-			
+		//if collision vector is same as vector(center to player) then player is moving away from the collision
+		if(cameraCenterToPlayer.x > 0 && ContactDir.x > 0){
 			isColliding = false;
-			
+		}else if(cameraCenterToPlayer.x < 0 && ContactDir.x < 0){
+			isColliding = false;
 		};
 		
 		
