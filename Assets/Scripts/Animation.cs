098774912a34f66cc9419c8f08a58f32eada33c9
Jessica Lee <jessicall_2@msn.com>
2013-08-18 04:26:01 +0200

    Här är en jättefin animation

---
+++ Assets/Scripts/Animation.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using System.Collections;
+
+public class Animation : MonoBehaviour {
+	
+	public float Animations; //Antal animationer på spriteSheeten
+	public float Frames;
+	public float Frametime;
+	public float distanceDelta; //Hur mycket storleken på spriten ska ändras beroende av avståndet
+	Vector3 spriteSize;
+	float xOffset;
+	float yOffset;
+	float currentFrame;
+	float clock;
+	
+	
+	void Start () {
+		
+		spriteSize = transform.localScale;//Storleken på objektet
+		
+		renderer.material.mainTextureScale = new Vector2(1,1/Animations); //Skalar av höjden(Y) till en frame
+		
+		currentFrame = 0; 
+		clock = Time.time; //Startar klockan
+		
+		xOffset = 0;
+	}
+	
+	
+	void Update () {
+		
+		if(Time.time - clock > Frametime){ //När frametimen passerat
+			
+			if(currentFrame < Frames - 1){
+				
+				currentFrame+=1;
+			}
+			
+			else{
+			
+				currentFrame=0; 	
+			}
+			
+			xOffset = currentFrame/Frames;
+			
+			renderer.material.mainTextureOffset = new Vector2(xOffset,yOffset); //Sätt nästa frame
+			
+			clock = Time.time; //Starta om klockan
+		}
+		
+		transform.localScale = spriteSize/(transform.position.z/distanceDelta); //Bildens storlek anpassas efter djupet(Z)
+	}
+	
+	
+	public void setAnimation(Vector2 direction){
+		
+		float degrees = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg; //omvandlar riktningskoordinaterna till antalet grader riktningen går i
+		yOffset = (degrees/(360/Animations))/Animations;
+		
+		renderer.material.mainTextureOffset = new Vector2(xOffset,yOffset); //sätter nuvarande offsetY
+	}
+}
