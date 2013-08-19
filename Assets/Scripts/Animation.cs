67dea6da38eb42545596391a1da15aa9b6d4c8e5
Jessica Lee <jessicall_2@msn.com>
2013-08-19 22:56:55 +0200

    Nu har den en stillastående också jättefint

--- Assets/Scripts/Animation.cs
+++ Assets/Scripts/Animation.cs
@@ -12,51 +12,64 @@ public class Animation : MonoBehaviour {
 	float yOffset;
 	float currentFrame;
 	float clock;
+	bool walk;
 	
 	
 	void Start () {
 		
 		spriteSize = transform.localScale;//Storleken på objektet
-		
-		renderer.material.mainTextureScale = new Vector2(1,1/Animations); //Skalar av höjden(Y) till en frame
-		
+		renderer.material.mainTextureScale = new Vector2(1/Frames,1/Animations); //Skalar av höjden(Y) till en frame
 		currentFrame = 0; 
 		clock = Time.time; //Startar klockan
-		
 		xOffset = 0;
+		yOffset = 0;
+		walk = false;
 	}
 	
 	
 	void Update () {
 		
-		if(Time.time - clock > Frametime){ //När frametimen passerat
+		if(walk){
 			
-			if(currentFrame < Frames - 1){
+			if(Time.time - clock > Frametime){ //När frametimen passerat
+			
+				if(currentFrame < Frames - 1){
 				
-				currentFrame+=1;
+					currentFrame+=1;
+				}
+			
+				else{
+			
+					currentFrame=0; 	
+				}
+			
+				xOffset = currentFrame/Frames;
+			
+				renderer.material.mainTextureOffset = new Vector2(xOffset,yOffset); //Sätt nästa frame
+			
+				clock = Time.time; //Starta om klockan
 			}
-			
-			else{
-			
-				currentFrame=0; 	
-			}
-			
-			xOffset = currentFrame/Frames;
-			
-			renderer.material.mainTextureOffset = new Vector2(xOffset,yOffset); //Sätt nästa frame
-			
-			clock = Time.time; //Starta om klockan
 		}
 		
 		transform.localScale = spriteSize/(transform.position.z/distanceDelta); //Bildens storlek anpassas efter djupet(Z)
 	}
 	
 	
-	public void setAnimation(Vector2 direction){
+	public void setWalkAnimation(Vector2 direction){
 		
 		float degrees = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg; //omvandlar riktningskoordinaterna till antalet grader riktningen går i
-		yOffset = (degrees/(360/Animations))/Animations;
+		
+		yOffset = (degrees/(360/Animations))/Animations; 
 		
 		renderer.material.mainTextureOffset = new Vector2(xOffset,yOffset); //sätter nuvarande offsetY
+		
+		walk = true;
+	}
+	
+	public void setStandAnimation(){
+	
+		renderer.material.mainTextureOffset = new Vector2(0,yOffset);	
+		
+		walk = false;
 	}
 }
