f79249b1a7e4ad91f448695902fb61d5da1081e9
Jessica Lee <jessicall_2@msn.com>
2013-10-16 17:06:16 +0200

    NU KOMMER ALLT DÖ

--- Assets/Scripts/Animationator.cs
+++ Assets/Scripts/Animationator.cs
@@ -7,7 +7,8 @@ public class Animationator : MonoBehaviour {
 	public float Frames;
 	public float Frametime;
 	public float distanceDelta; //Hur mycket storleken på spriten ska ändras beroende av avståndet
-	Vector3 spriteSize;
+	public float groundDepth;
+	Vector3 orgSpriteSize;
 	float xOffset;
 	float yOffset;
 	float currentFrame;
@@ -18,7 +19,7 @@ public class Animationator : MonoBehaviour {
 	
 	void Start () {
 		
-		spriteSize = transform.localScale;//Storleken på objektet
+		orgSpriteSize = transform.GetChild(0).localScale;//Storleken på objektet
 		playerMat = transform.GetChild(0).renderer.material;
 		playerMat.mainTextureScale = new Vector2(1/Frames,1/Animations); //Skalar av höjden(Y) till en frame
 		currentFrame = 0; 
@@ -54,16 +55,41 @@ public class Animationator : MonoBehaviour {
 			}
 		}
 		
+		float spriteSize = distanceDelta * transform.GetChild(0).position.z;
+		
+		transform.GetChild(0).localScale = new Vector3(orgSpriteSize.x - orgSpriteSize.x*spriteSize, orgSpriteSize.z - orgSpriteSize.z*spriteSize, orgSpriteSize.z);
+		
 		//transform.localScale = spriteSize/(transform.position.z/distanceDelta); //Bildens storlek anpassas efter djupet(Z)
 	}
 	
 	
 	public void setWalkAnimation(Vector2 direction){
 		
-		float degrees = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg; //omvandlar riktningskoordinaterna till antalet grader riktningen går i
+		//float degrees = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg; //omvandlar riktningskoordinaterna till antalet grader riktningen går i
 		//Debug.Log (degrees);
 		
-		yOffset = (degrees/(360/Animations))/Animations; 
+		//yOffset = (degrees/(360/Animations))/Animations; 
+		
+		
+		//världens fulaste jävla cplösning nedanför, ser dessutom skitfult ut
+		if(direction.y < 0.5 && direction.y > -0.5){
+			if(direction.x > 0){
+				yOffset = 0.25f;
+			}
+			if(direction.x < 0){
+				yOffset = 0.75f;
+			}
+		}else{
+			if(direction.y < 0){
+				
+				yOffset = 0.5f;
+			}
+			if(direction.y > 0){
+				
+				yOffset = 0;
+			}
+		}		
+
 		
 		playerMat.mainTextureOffset = new Vector2(xOffset,yOffset); //sätter nuvarande offsetY
 		
