4218a986bd91cc2d72ecde308411eabf42c6a17e
Sean <conk3r_xd@hotmail.com>
2013-12-08 17:25:09 +0100

    Fade

--- Assets/Scripts/LevelInitation.cs
+++ Assets/Scripts/LevelInitation.cs
@@ -7,12 +7,23 @@ public class LevelInitation : MonoBehaviour {
 	
 	public string spawnpointName = "defaultSpawn";
 	
+	public Texture2D blackTexture;
+	private float alphaFadeValue = 0.0f;
+	public bool initiateFade = false;
+	private string nextLevel;
 
 	// Use this for initialization
 	void Start () {
 		DontDestroyOnLoad(transform.gameObject);
-
 		OnLevelWasLoaded(); //kan ställa till saker sen!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DANGERZONE
+	}
+	
+	void Update(){
+		if(initiateFade){
+			FadeIn();
+		}else{			//it might be expensive to always diminish the fadevalue...
+			FadeOut();
+		}
 	}
 	
 	void OnLevelWasLoaded(){
@@ -32,8 +43,34 @@ public class LevelInitation : MonoBehaviour {
 		player.GetComponent<Pathfinding>().walkmesh = floor;
 	}
 	
+	//spawnpoint in new level
 	void setSpawnpoint(string spawnName){
 		spawnpointName = spawnName;
 	}
+
+	void LoadLevel(string nextLevelName){
+		initiateFade = true;
+		//can we always be sure that this is the next level and not the current?
+		nextLevel = nextLevelName;
+	}
 	
+	void FadeIn(){
+		alphaFadeValue = Mathf.Clamp01(alphaFadeValue + (Time.deltaTime * 2));
+		if(alphaFadeValue > 0.99){
+			Application.LoadLevel(nextLevel);
+			initiateFade = false;
+		}
+	}
+	
+	void FadeOut(){
+		if(alphaFadeValue > 0.01f){
+			alphaFadeValue = Mathf.Clamp01(alphaFadeValue - (Time.deltaTime * 2));
+		}
+	}
+	
+	//create new color every GUI tick might be expensive..
+	void OnGUI(){
+		GUI.color = new Color(0,0,0,alphaFadeValue);
+		GUI.DrawTexture(new Rect(0,0,Screen.width, Screen.height), blackTexture);
+	}
 }
