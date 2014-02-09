d5bd70e2b48b54a6b8f7d8cfe9286fbcaa8de9e6
Sean <conk3r_xd@hotmail.com>
2014-02-09 17:04:57 +0100

    death

--- Assets/Scripts/StromAvbrott.cs
+++ Assets/Scripts/StromAvbrott.cs
@@ -5,9 +5,24 @@ public class StromAvbrott : MonoBehaviour {
 
 	public bool powerON = true;
 	public bool avbrott = false;
+	public Transform[] reactiveObjects;
+
+	public bool asdhjabsdkja = true;
 
 	void Start(){
 		setOn();
+	}
+
+	void Update(){
+		if(Input.GetKey(KeyCode.J)){
+			//fråga sean
+			if(asdhjabsdkja){
+				powerOut();
+				Debug.Log("powerout!");
+				asdhjabsdkja = false;
+			}
+
+		}
 	}
 
 	void OnEnable(){
@@ -41,5 +56,13 @@ public class StromAvbrott : MonoBehaviour {
 		foreach(GameObject g in withtag){
 			g.renderer.enabled =!powerON;
 		}
+	}
+
+	void powerOut(){
+		for(int i  = 0; i < reactiveObjects.Length; i++){
+			reactiveObjects[i].SendMessage("setPowerOut", false);
+		}
+		avbrott = true;
+		togglePowerON();
 	}
 }
