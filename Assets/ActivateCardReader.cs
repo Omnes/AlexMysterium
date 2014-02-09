37305d3a7ddd6154e15a489188f051ac32c6176e
Robin Iderström <robin.iderstrom@live.se>
2014-02-09 23:16:45 +0100

    asdsad

---
+++ Assets/ActivateCardReader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateCardReader : MonoBehaviour {
+	public Transform trans;
+	
+	void ActivateStuff(){
+		trans.GetComponent<Cardreader>().enabled = true;
+	}
+
+	void DeactivateStuff(){
+		trans.GetComponent<Cardreader>().enabled = false;
+	}
+}
