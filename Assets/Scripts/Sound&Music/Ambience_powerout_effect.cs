eaa1263c34b29cb89782e65720cee11b5b5fe90a
Jessica Lee <jessicall_2@msn.com>
2014-02-14 16:38:11 +0100

    bättre questlog

--- Assets/Scripts/Sound&Music/Ambience_powerout_effect.cs
+++ Assets/Scripts/Sound&Music/Ambience_powerout_effect.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Ambience_powerout_effect : MonoBehaviour {
 
-	float poweroutVolumeBuff = 1.2;
+	float poweroutVolumeBuff = 1.2f;
 	float speakerVolume;
 	AudioSource speaker;
 	// Use this for initialization
@@ -30,14 +30,14 @@ public class Ambience_powerout_effect : MonoBehaviour {
 		speakerVolume = speaker.volume;
 		float goalVolume = speakerVolume*poweroutVolumeBuff;
 		while(speaker.volume < goalVolume){
-			speaker.volume += 0.1*Time.deltaTime;
+			speaker.volume += 0.1f*Time.deltaTime;
 		}
 	}
 
 	void fadeOut(){
 		float goalVolume = speakerVolume;
 		while(speaker.volume > goalVolume){
-			speaker.volume -= 0.1*Time.deltaTime;
+			speaker.volume -= 0.1f*Time.deltaTime;
 		}
 	}
 }
